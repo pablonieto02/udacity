43ffd764b0c01adb29d1ea321b4a55699b53c688
Pablo <pablonieto02@gmail.com>
2018-12-29 00:07:39 -0300

    teeeste

--- FDSII/Projeto_01/scripts.sql
+++ FDSII/Projeto_01/scripts.sql
@@ -1,19 +1,16 @@
 -- Pergunta 1: Quais países possuem mais faturas?
-
 SELECT BillingCountry, COUNT(invoiceid) AS Invoices
 FROM Invoice
 GROUP BY BillingCountry
 ORDER BY COUNT(invoiceid) DESC
 
 -- Pergunta 2: Qual cidade tem os melhores clientes?
-
 SELECT BillingCity, SUM(total) AS Invoices
 FROM Invoice
 GROUP BY BillingCity
 ORDER BY SUM(total) DESC
 
 -- Pergunta 3: Quem é o melhor cliente?
-
 SELECT c.customerid, c.FirstName, SUM(i.total)
 FROM Invoice i
 INNER JOIN Customer c ON c.customerid = i.customerid
