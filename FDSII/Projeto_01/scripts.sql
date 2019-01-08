9f8ceac4ca9532b8c91a84764117ab6382a8e878
pablonieto02 <32034105+pablonieto02@users.noreply.github.com>
2019-01-08 13:45:50 -0300

    Grupo de Questões 2

--- FDSII/Projeto_01/scripts.sql
+++ FDSII/Projeto_01/scripts.sql
@@ -16,3 +16,31 @@ FROM Invoice i
 INNER JOIN Customer c ON c.customerid = i.customerid
 GROUP BY c.customerid, c.FirstName
 ORDER BY SUM(i.total) DESC
+
+-- Conjunto de Perguntas 2
+/*Pergunta 1 - Use sua consulta para retornar o e-mail, nome, sobrenome e gênero de todos os ouvintes de Rock. Retorne sua lista ordenada alfabeticamente
+por endereço de e-mail, começando por A. Você consegue encontrar um jeito de lidar com e-mails duplicados para que ninguém receba vários
+e-mails?*/
+SELECT DISTINCT c.Email, c.FirstName, c.LastName, g.Name
+FROM Invoice i
+INNER JOIN Customer c ON c.customerid = i.customerid
+INNER JOIN InvoiceLine il ON il.Invoiceid = i.Invoiceid
+INNER JOIN Track t ON t.Trackid = il.Trackid
+INNER JOIN Genre g ON g.GenreId = t.GenreId
+WHERE g.GenreId = 1
+ORDER BY c.Email
+
+/*
+Pergunta 2: Quem está escrevendo as músicas de rock?
+Agora que sabemos que nossos clientes amam rock, podemos decidir quais músicos convidar para tocar no show.
+Vamos convidar os artistas que mais escreveram as músicas de rock em nosso banco de dados. Escreva uma consulta que retorna o nome do
+Artist (artista) e a contagem total de músicas das dez melhores bandas de rock.
+*/
+SELECT a.Artistid, a.Name, COUNT(t.Trackid) Songs
+FROM Track t
+INNER JOIN Genre g ON g.GenreId = t.Genreid
+INNER JOIN Album alb ON alb.Albumid = t.Albumid
+INNER JOIN Artist a ON a.Artistid = alb.Artistid
+WHERE g.GenreId = 1
+GROUP BY a.Artistid, a.Name
+ORDER BY Songs DESC
