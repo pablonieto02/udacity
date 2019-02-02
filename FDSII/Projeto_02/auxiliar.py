df3d0bfc9f5a10700618b3cfd720b8b5b06988ea
Your Name <you@example.com>
2019-02-02 15:30:47 -0300

    a

---
+++ FDSII/Projeto_02/auxiliar.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import pandas as pd
+
+df_img_pred = pd.read_csv('image-predictions.tsv', sep='\t')
+
+folder_name = 'dogs_jpg'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+for url in df_img_pred['jpg_url']:
+    response = requests.get(url)
+    with open(os.path.join(folder_name, url.split('/')[-1]), mode = 'wb') as file:
+              file.write(response.content)
+print(os.listdir(folder_name))
