66b27d665171a5fad1cfb1e417df93e569db8691
Pablo Nieto <pablonieto02@gmail.com>
2019-04-02 21:37:21 -0300

    a

--- FDSII/Projeto_02/Teste_Twitter.py
+++ FDSII/Projeto_02/Teste_Twitter.py
@@ -1,4 +1,5 @@
 import tweepy
+import json
 
 consumer_key = '1oQF3Zj6t7R3ylTZqgqXgOufn'
 consumer_secret = '20gmonFIHe9eBegDQlH0wqZTxx434nK4G8fOBru6j2bDGurmBK'
@@ -9,3 +10,14 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
+
+tweet = api.get_status(892420643555336193)
+#print(tweet._json)
+
+with open('data.txt', 'w') as outfile:
+    json.dump(tweet._json, outfile)
+
+
+#retweet_count
+#favorite_count
+#retweeted
