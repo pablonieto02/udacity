1f325f5a075f2015d55c481a11315c92e5827298
Pablo Nieto <pablonieto02@gmail.com>
2019-04-06 21:51:26 -0300

    a

--- FDSII/Projeto_02/read_json.py
+++ FDSII/Projeto_02/read_json.py
@@ -1,6 +1,6 @@
 import json
 
-with open('tweet_json.json') as json_file:  
+with open('tweet_json.text') as json_file:  
     data = json.load(json_file)
     for row in data['tweet']:
         print(row['id'])
